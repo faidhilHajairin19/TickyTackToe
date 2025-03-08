cd2f3486d1230b694b2612d5ad1b8ef08af7ca50
faidhilHajairin19 <hajairinfaidhil19@gmail.com>
2025-03-08 22:49:54 +0800

    Create TugasApp.swift

---
+++ TugasApp.swift
@@ -0,0 +1,17 @@
+//
+//  TugasApp.swift
+//  Tugas
+//
+//  Created by Faidhil Hajairin on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TugasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            homeScreen()
+        }
+    }
+}
