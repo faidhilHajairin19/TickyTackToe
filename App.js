ff2d6e00b86b00824f39734b0de66f3ad07f6f9e
faidhilHajairin19 <hajairinfaidhil19@gmail.com>
2025-04-30 00:29:18 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeStackNavigator from "./src/navigations/navigator"
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <HomeStackNavigator />
+    </NavigationContainer>
+  )
+}
+export default App;
